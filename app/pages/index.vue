f99a8666880ca9e2b793f54b570c8f4090f31dc5
Eric Robinson <eric.z.robinson@gmail.com>
2024-12-25 13:06:50 -0500

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -31,7 +31,7 @@ useSeoMeta({
   <div>
 
     <LazyUContainer class="py-12 text-center mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl flex flex-col my-0">
-      <NuxtImg src="icons/512.png" alt="Mass GUID Converter" class="w-32 h-32 sm:w-72 sm:h-72 mx-auto" />
+      <img src="~public/icons/512.png" alt="Mass GUID Converter" class="w-32 h-32 sm:w-72 sm:h-72 mx-auto" />
       <div class="dark:text-primary-400 text-primary-500 text-2xl md:text-6xl xs:text-2xl sm:text-4xl font-black">
         <h1>Mass GUID Converter</h1>
       </div>
